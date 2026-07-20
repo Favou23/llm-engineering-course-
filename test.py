7a3744bf90bd9bc2ebb034e052f55011fe572843
favour <shaibfavour26@gmail.com>
2026-07-20 23:44:06 +0100

    trying out a new branch

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-import os 
-from dotenv import load_dotenv 
-load_dotenv()
-from openai import OpenAI
+# import os 
+# from dotenv import load_dotenv 
+# load_dotenv()
+# from openai import OpenAI
 
-client = OpenAI()
+# client = OpenAI()
 
 
-messages = [
-    {"role": "system", "content": "youre a professional software engineeri"},
-    {"role":"user", "content": "what is software engineering?"}
-]
+# messages = [
+#     {"role": "system", "content": "youre a professional software engineeri"},
+#     {"role":"user", "content": "what is software engineering?"}
+# ]
 
-response =  client.chat.completions.create(
-    model = "gpt-5-nano", messages = messages
-),
+# response =  client.chat.completions.create(
+#     model = "gpt-5-nano", messages = messages
+# ),
 
-response.choice[0].message.content
+# response.choice[0].message.content
 
 
 
@@ -46,17 +46,40 @@ from dotenv import load_dotenv
 load_dotenv()
 from openai import OpenAI
 
-openai=OpenAI()
 
 
-messages= [
-    {"role":"system", "content": "youre a professional software engineer"},
-    {"role":"user", "content":"what is software engineering?"}
+# openai=OpenAI()
+
+
+# messages= [
+#     {"role":"system", "content": "youre a professional software engineer"},
+#     {"role":"user", "content":"what is software engineering?"}
+# ]
+
+# response = openai.chat.completions.create(
+#     model = "gpt 4o-mini", messages= "messages"
+# )
+
+# output = response.choices[0].message.content
+# print (output)
+
+
+
+###runnning open source models with olama 
+
+OLLAMA_BASE_URL = "http://localhost:11434/v1"
+
+ollama = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
+api_key = os.getenv("OLLAMA_API_KEY")
+
+message = [
+    {"role":"system", "content":"youre a rofessional software engineer"},
+    {"role": "user", "content":"what is sotware engneering? explain in maximum detail"}
 ]
 
-response = openai.chat.completions.create(
-    model = "gpt 4o-mini", messages= "messages"
+response = ollama.chat.completions.create(
+    model ="llama3.2:3b", messages = message
 )
 
-output = response.choices[0].message.content
-print (output)
+output= response.choices[0].message.content
+print(output) 
